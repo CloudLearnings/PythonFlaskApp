28733ffe3e21e90b1eda623c24b100437c416b73
Naveen Joshi <navijoshi86@gmail.com>
2023-09-08 19:26:51 +0530

    updated app to call mongodb

--- src/app.py
+++ src/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, jsonify
+import pymongo
 
 app = Flask(__name__)
+
+# Create a MongoDB client
+mongo_client = pymongo.MongoClient("mongodb://172.17.0.2:27017")  # Replace <mongo_ip> with the actual IP address
+
+# Define a function to fetch database names
+def get_database_names():
+    return mongo_client.list_database_names()
 
 @app.route('/')
 def index():
@@ -10,6 +18,11 @@ def index():
 def hello_json():
     message = {"message": "Hello, World!"}
     return jsonify(message)
+
+@app.route('/fetch_dbs')
+def fetch_dbs():
+    db_names = get_database_names()
+    return jsonify({"databases": db_names})
 
 if __name__ == '__main__':
     app.run(debug=True)
